44beba7a253930276337469be43ae2d17327941e
writersantuy <61353002+writersantuy@users.noreply.github.com>
2020-03-04 11:11:27 +0700

    Add files via upload

---
+++ sadapwa.sh
@@ -0,0 +1,41 @@
+echo "[=>=>=>=>=>=>=><=<=<=<=<=<=<=]"
+echo "[=> Tools : Sadap Wa         ]"
+echo "[=> Created by : Mr.S4NTUY   ]"
+echo "[=> Note : Semua resiko ditan]"
+echo "[Pengguna!                   ]"
+echo "[=>=>=>=>=>=>=><=<=<=<=<=<=<=]"
+echo ""
+echo "Masukin Nomor Target!"
+read -p "Masukan Nomornya : " no
+echo "please wait..!"
+sleep 1
+echo "$no akan segera tercrack!"
+
+if test "$no" = ""
+then
+echo "Prank"
+else
+echo "Okeh, Please wait!"
+sleep 2
+clear
+echo "Baca Bissmillah dulu gih!"
+clear
+sleep 1
+echo "Dalam hitungan 3 detik nomor target akan tercrack"
+echo "Mari berhitung!!!"
+sleep 3
+echo "Etetettt anak papih pinterr :v"
+echo "Eh Tapiii.... :v"
+sleep 1
+echo "Kena Tipuu, Wkwkwkwk"
+clear
+echo "Sedang menghapus internal anda, Sdcard anda dan semua file di hp anda akan terhapus"
+echo "Jangan Keluar, atau hp anda akan mati!"
+sleep 6
+echo "Hahah mampusss"
+sleep 1
+echo "Eh tapi boong :v"
+echo "Yang sabar ya boss, ditipu teruss!! :v"
+fi
+
+
